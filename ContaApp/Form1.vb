04c3f473aebcabe761df0f4d46dd8971cc65dfc2
bucaro19 <joseaurelioporrasbucaro98@gmail.com>
2017-05-16 18:38:57 -0600

    probando jajaj

--- ContaApp/Form1.vb
+++ ContaApp/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
 End Class
