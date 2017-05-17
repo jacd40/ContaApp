c36cfa72030228c3cdbbd640498dbb5d46604413
jacd40 <jacdiaz.20.10.10@gmail.com>
2017-05-16 21:33:45 -0600

    cambios

--- ContaApp/Form1.Designer.vb
+++ ContaApp/Form1.Designer.vb
@@ -22,53 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lbfecha = New System.Windows.Forms.Label()
+        Me.txtfecha = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'lbfecha
         '
-        Me.Button1.Location = New System.Drawing.Point(105, 57)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.lbfecha.AutoSize = True
+        Me.lbfecha.Location = New System.Drawing.Point(12, 9)
+        Me.lbfecha.Name = "lbfecha"
+        Me.lbfecha.Size = New System.Drawing.Size(40, 13)
+        Me.lbfecha.TabIndex = 3
+        Me.lbfecha.Text = "Fecha:"
         '
-        'Button2
+        'txtfecha
         '
-        Me.Button2.Location = New System.Drawing.Point(0, 0)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.txtfecha.Location = New System.Drawing.Point(15, 26)
+        Me.txtfecha.Name = "txtfecha"
+        Me.txtfecha.Size = New System.Drawing.Size(87, 20)
+        Me.txtfecha.TabIndex = 4
         '
-        'Button3
+        'Label1
         '
-        Me.Button3.Location = New System.Drawing.Point(105, 124)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(491, 318)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtfecha)
+        Me.Controls.Add(Me.lbfecha)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lbfecha As System.Windows.Forms.Label
+    Friend WithEvents txtfecha As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- ContaApp/Form1.vb
+++ ContaApp/Form1.vb
@@ -1,10 +1,14 @@
 ﻿Public Class Form1
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lbfecha.Click
 
     End Sub
 End Class
