bddecc9c4b77b0cfb91167ef00b40ec14bb5296b
jacd40 <jacdiaz.20.10.10@gmail.com>
2017-05-16 21:44:49 -0600

    diario

--- ContaApp/Form1.Designer.vb
+++ ContaApp/Form1.Designer.vb
@@ -25,6 +25,14 @@ Partial Class Form1
         Me.lbfecha = New System.Windows.Forms.Label()
         Me.txtfecha = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtcodigo = New System.Windows.Forms.TextBox()
+        Me.lbcuenta = New System.Windows.Forms.Label()
+        Me.cmbcuenta = New System.Windows.Forms.ComboBox()
+        Me.txtmonto = New System.Windows.Forms.TextBox()
+        Me.lbmonto = New System.Windows.Forms.Label()
+        Me.lbtransaccion = New System.Windows.Forms.Label()
+        Me.cmbtransaccion = New System.Windows.Forms.ComboBox()
+        Me.btnagregar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'lbfecha
@@ -48,15 +56,89 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(15, 53)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
         Me.Label1.TabIndex = 5
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Codigo:"
+        '
+        'txtcodigo
+        '
+        Me.txtcodigo.Location = New System.Drawing.Point(64, 53)
+        Me.txtcodigo.Name = "txtcodigo"
+        Me.txtcodigo.Size = New System.Drawing.Size(87, 20)
+        Me.txtcodigo.TabIndex = 6
+        '
+        'lbcuenta
+        '
+        Me.lbcuenta.AutoSize = True
+        Me.lbcuenta.Location = New System.Drawing.Point(171, 53)
+        Me.lbcuenta.Name = "lbcuenta"
+        Me.lbcuenta.Size = New System.Drawing.Size(44, 13)
+        Me.lbcuenta.TabIndex = 7
+        Me.lbcuenta.Text = "Cuenta:"
+        '
+        'cmbcuenta
+        '
+        Me.cmbcuenta.FormattingEnabled = True
+        Me.cmbcuenta.Location = New System.Drawing.Point(221, 50)
+        Me.cmbcuenta.Name = "cmbcuenta"
+        Me.cmbcuenta.Size = New System.Drawing.Size(258, 21)
+        Me.cmbcuenta.TabIndex = 8
+        '
+        'txtmonto
+        '
+        Me.txtmonto.Location = New System.Drawing.Point(64, 89)
+        Me.txtmonto.Name = "txtmonto"
+        Me.txtmonto.Size = New System.Drawing.Size(87, 20)
+        Me.txtmonto.TabIndex = 10
+        '
+        'lbmonto
+        '
+        Me.lbmonto.AutoSize = True
+        Me.lbmonto.Location = New System.Drawing.Point(15, 89)
+        Me.lbmonto.Name = "lbmonto"
+        Me.lbmonto.Size = New System.Drawing.Size(40, 13)
+        Me.lbmonto.TabIndex = 9
+        Me.lbmonto.Text = "Monto:"
+        '
+        'lbtransaccion
+        '
+        Me.lbtransaccion.AutoSize = True
+        Me.lbtransaccion.Location = New System.Drawing.Point(171, 89)
+        Me.lbtransaccion.Name = "lbtransaccion"
+        Me.lbtransaccion.Size = New System.Drawing.Size(69, 13)
+        Me.lbtransaccion.TabIndex = 11
+        Me.lbtransaccion.Text = "Transaccion:"
+        '
+        'cmbtransaccion
+        '
+        Me.cmbtransaccion.FormattingEnabled = True
+        Me.cmbtransaccion.Location = New System.Drawing.Point(246, 89)
+        Me.cmbtransaccion.Name = "cmbtransaccion"
+        Me.cmbtransaccion.Size = New System.Drawing.Size(121, 21)
+        Me.cmbtransaccion.TabIndex = 12
+        '
+        'btnagregar
+        '
+        Me.btnagregar.Location = New System.Drawing.Point(394, 89)
+        Me.btnagregar.Name = "btnagregar"
+        Me.btnagregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnagregar.TabIndex = 13
+        Me.btnagregar.Text = "Agregar"
+        Me.btnagregar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(491, 318)
+        Me.Controls.Add(Me.btnagregar)
+        Me.Controls.Add(Me.cmbtransaccion)
+        Me.Controls.Add(Me.lbtransaccion)
+        Me.Controls.Add(Me.txtmonto)
+        Me.Controls.Add(Me.lbmonto)
+        Me.Controls.Add(Me.cmbcuenta)
+        Me.Controls.Add(Me.lbcuenta)
+        Me.Controls.Add(Me.txtcodigo)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtfecha)
         Me.Controls.Add(Me.lbfecha)
@@ -69,5 +151,13 @@ Partial Class Form1
     Friend WithEvents lbfecha As System.Windows.Forms.Label
     Friend WithEvents txtfecha As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtcodigo As System.Windows.Forms.TextBox
+    Friend WithEvents lbcuenta As System.Windows.Forms.Label
+    Friend WithEvents cmbcuenta As System.Windows.Forms.ComboBox
+    Friend WithEvents txtmonto As System.Windows.Forms.TextBox
+    Friend WithEvents lbmonto As System.Windows.Forms.Label
+    Friend WithEvents lbtransaccion As System.Windows.Forms.Label
+    Friend WithEvents cmbtransaccion As System.Windows.Forms.ComboBox
+    Friend WithEvents btnagregar As System.Windows.Forms.Button
 
 End Class
